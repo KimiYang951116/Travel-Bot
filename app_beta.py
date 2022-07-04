e7c14ca0197fb79010536e27a2ea3407b7801eae
KimiYang951116 <planrandanimalandinsectjh@gmail.com>
2022-07-04 08:57:09 +0800

    g

--- app_beta.py
+++ app_beta.py
@@ -10,7 +10,7 @@ from linebot.exceptions import InvalidSignatureError
 from linebot.models import ConfirmTemplate, URITemplateAction, URIAction, ButtonComponent, FlexSendMessage, IconComponent, ImageComponent, TextComponent, BoxComponent, BubbleContainer, MessageTemplateAction, CarouselTemplate, CarouselColumn, MessageEvent, TextMessage, LocationMessage, TextSendMessage, QuickReply, QuickReplyButton, MessageAction, ImageSendMessage, StickerSendMessage, LocationSendMessage, TemplateSendMessage
 
 # My module
-from sql import AddUserInfo, CheckUserExistance, GetUserInfo
+from sql import AddUserInfo, CheckUserExistance, GetUserInfo, UpdateUserInfo
 from config import LINE_API_KEY, WEBHOOK_HANDLER, RULES
 from find_places import find_restaurant
 
@@ -32,7 +32,54 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
+    connection = pymysql.connect(
+        host="us-cdbr-east-05.cleardb.net",
+        user="b5f2e205874506",
+        password="3291697e",
+        db="heroku_b2cccf87a825db4",
+        charset='utf8mb4',
+        cursorclass=pymysql.cursors.DictCursor
+    )
     etext = event.message.text
+    user_id = event.source.user_id
+    user_name = line_bot_api.get_profile(user_id)
+    multimessage = []
+    is_exist = CheckUserExistance(connection, user_id)
+    if is_exist == False:
+        multimessage.append(TextSendMessage(text=f'你目前不再資料庫中，現在將立即為你新增'))
+        AddUserInfo(connection, user_id)
+        multimessage.append(TextSendMessage(text=f'已經成功將你加入資料庫\n歡迎你{user_name.display_name}，接著請同意我們的使用條款'))
+    is_agree = GetUserInfo(connection, user_id, 'service')
+    if is_agree == 0:
+        multimessage.append(TextSendMessage(text="你尚未同意我們的使用條款，請先同意我們的條款\n"\
+        "條款網址如下"))
+        multimessage.append(TextSendMessage(text=RULES))
+        multimessage.append(TemplateSendMessage(
+            alt_text = '同意使用條款?',
+            template = ConfirmTemplate(
+                text = '你是否同意我們的使用條款?',
+                actions = [
+                    MessageTemplateAction(
+                        label = '我同意',
+                        text = '/service/1'
+                    ),
+                    MessageTemplateAction(
+                        label = '我不同意',
+                        text = '/service/0'
+                    )
+                ]
+            )
+        ))
+        line_bot_api.reply_message(event.reply_token, multimessage)
+    elif etext.startswith('/service'):
+        proetext = etext.split('/')
+        proetext = proetext[2]
+        if proetext == 1:
+            UpdateUserInfo(connection, user_id, 'service', 1)
+            line_bot_api.reply_message(event.reply_token, TextSendMessage(text = 'OK，你現在可以開始使用Travel Bot 的所有功能'))
+        else:
+            line_bot_api.reply_message(event.reply_token, TextSendMessage(text = '很抱歉，由於你不同意我們的條款，我們無法為你提供服務，同意條款以獲得服務'))
+
     
 
         
@@ -66,11 +113,11 @@ def handle_message(event):
                 actions = [
                     MessageTemplateAction(
                         label = '我同意',
-                        text = '/service/yes'
+                        text = '/service/1'
                     ),
                     MessageTemplateAction(
                         label = '我不同意',
-                        text = '/service/no'
+                        text = '/service/0'
                     )
                 ]
             )
