283cdd4530bfab7a597ae2ac3313e15b2e94fdd0
Kimi Yang <plantandanimalandinsectjh@gmail.com>
2022-07-29 18:23:03 +0800

    f

--- dev/remark.sh
+++
@@ -1,6 +0,0 @@
-curl -v -X POST https://api-data.line.me/v2/bot/richmenu/richmenu-2daf84e40cf176b9af4420ed04ea581c/content \
--H "Authorization: Bearer /NWkZeGHgzhXfj6UzLDYIf+xv5n3DHk4zB+YrNB2iIZafn55A44Eqfk3qOZClkK2webGFYYLpf5pQWGXzD0BKAJfJsJm8zDWyNSMnQ6pcgGwtF4j4hHelBnU5kgZUhP0TTiCXJ9fewaSpB+U4RIWawdB04t89/1O/w1cDnyilFU=" \
--H "Content-Type: image/jpeg" \
--T image.jpeg
-
-curl -v -X POST https://api-data.line.me/v2/bot/user/Uc10a26bd60f68b67b4db78d4ee14b8d2/richmenu/richmenu-2daf84e40cf176b9af4420ed04ea581c
--- ha.py
+++
@@ -1,5 +0,0 @@
-from config import LINE_API_KEY, default_richmenu_id
-from linebot import LineBotApi
-from linebot.models import *
-line_bot_api = LineBotApi(LINE_API_KEY)
-line_bot_api.link_rich_menu_to_user('Uc10a26bd60f68b67b4db78d4ee14b8d2', default_richmenu_id)
