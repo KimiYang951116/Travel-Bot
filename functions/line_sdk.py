64e83c83c468d384e962244ffa732cafa9345535
Kimi Yang <plantandanimalandinsectjh@gmail.com>
2022-08-11 10:46:18 +0800

    g

--- functions/line_sdk.py
+++ functions/line_sdk.py
@@ -118,7 +118,7 @@ def make_bubble_component(place_name, detail_lst, now_latlong):
             ]
         ),
         hero=ImageComponent(
-            url=f'https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photo_reference={photo}&key=AIzaSyDIX1tgCL2g8bS9o9rT50G8GyPvY1cBNFE'
+            url=f'https://maps.googleapis.com/maps/api/place/photo?maxwidth=800&photo_reference={photo}&key=AIzaSyDIX1tgCL2g8bS9o9rT50G8GyPvY1cBNFE'
         ),
         body=BoxComponent(
             layout='vertical',
