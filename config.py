a15ffb2498126a902fbdccfb42d84925c806bc90
Kimi Yang <plantandanimalandinsectjh@gmail.com>
2022-08-22 11:38:30 +0800

    g

--- config.py
+++ config.py
@@ -10,7 +10,10 @@ RANKBY_DICT = {
     'all': 'prominence'
 }
 is_agreeTrue = 'OK，你現在可以開始使用\nTravel Bot 的所有功能'
-is_agreeFalse = '很抱歉，由於你不同意\n我們的同意事項，我們無法\n為你提供服務，同意我們\n的同意事項以獲得服務'
+is_agreeFalse = '''很抱歉，由於你不同意
+我們的同意事項，我們無法
+為你提供服務，同意我們
+的同意事項以獲得服務'''
 default_richmenu_id = 'richmenu-112595219b475989a0b0177473ffa70b'  # 'richmenu-338c645051065cceebc3b1c65ffc7b1b'  # noqa: E501
 no_location_richmenu_id = 'richmenu-391c55557497b3067bad78a6499cd2fc'  # 'richmenu-818171f764b0fdb76d8e4d896ee3efa0'  # noqa: E501
 choose_catagory_richmenu_id = 'richmenu-d066fd9f8ab16847ad0638f140cf21f8'  # noqa: E501
