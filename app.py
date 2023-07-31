a53f12852c4cd6e267dda497c04152cb8115062d
Kimi Yang <plantandanimalandinsectjh@gmail.com>
2023-07-31 14:22:53 +0800

    'g'

--- app.py
+++ app.py
@@ -178,12 +178,6 @@ def handle_postback_message(event):
         cursorclass=pymysql.cursors.DictCursor
     )
     user_id = event.source.user_id
-    if r.get(user_id) is None:
-        r.set(user_id, 1, ex=3)
-    else:
-        bubble = make_general_bubble_component('請不要連續點擊')
-        line_bot_api.reply_message(event.reply_token, FlexSendMessage(alt_text='請不要連續點擊', contents=bubble))
-        return
     edata = event.postback.data
     multimessage = []
     is_exist = CheckUserExistance(connection, user_id)
